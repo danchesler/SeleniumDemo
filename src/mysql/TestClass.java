1d3f5091c5509b889c728faa5bac876bdb98e649
danchesler <dan.cheeto@gmail.com>
2023-10-16 15:18:42 -0700

    dev branch

--- src/mysql/TestClass.java
+++ src/mysql/TestClass.java
@@ -4,12 +4,19 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		printThis();
+		
+		give5();
 
 	}
 	
 	public static void printThis()
 	{
 		System.out.println("cool");
+	}
+	
+	public static void give5()
+	{
+		System.out.println("5");
 	}
 
 }
